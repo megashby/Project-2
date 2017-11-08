3420345005d1af5d353bf080f6b1037f59d7510c
Meg Ashby <megashby@utexas.edu>
2017-11-07 22:19:35 -0600

    Add files via upload

--- protofunctions.js
+++ protofunctions.js
@@ -1,30 +1,32 @@
-
-var useranswers = new Array();
-// var answered = 0;
+var useranswers = Array();
 var correct = 0;
 
-function renderQuiz() {
+function createQuiz() {
   for(i=0;i<questions.length;i++) {
-    document.writeln('<p class="question">' + questions[i] + ' <span id="result_' + i + '"></span></p>');
-    for(j=0;j<choices[i].length;j++) {
-      document.writeln('<input type="radio" name="answer_' + i + '" value="' + choices[i][j] + '" id="answer_' + i + '_' + j + '" class="question_' + i + '" onclick="submitAnswer(' + i + ', this, \'question_' + i + '\')" /><label id="label_' + i + '_' + j + '" for="answer_' + i + '_' + j + '"> ' + choices[i][j] + '</label><br />');
+    document.writeln('<p class="question">'+questions[i]+' <p id="result_'+i+'"></p></p>');
+    for(j=0;j<3;j++) {
+      document.writeln('<input type="radio" name="answer_'+i+'" value="'+choices[i][j]+'" id="answer_'+i +'_'+j+'" class="question_'+i+'" onclick="submitQuestion('+i+', this, \'question_'+i+'\')" /><label id="label_'+i+'_'+j+'" for="answer_'+i+'_'+j+'"> '+choices[i][j]+'</label><br />');
     }
   }
-  document.writeln('<p><input type="submit" value="Show Score" onclick="showScore()" /></p><p style="display:none"></p>');
+  document.writeln('<p><input type="submit" value="Show Score" onclick="showTotalScore()" /></p><p style="display:none"></p>');
 }
 
-function submitAnswer(questionId, obj, classId) {
-  useranswers[questionId] = obj.value;
-  disableQuestion(classId);
-  showResult(questionId);
-  // answered++;
-  // alert(answered)
+function submitQuestion(questionNum, obj, classNum) {
+  useranswers[questionNum] = obj.value;
+  disableQuestion(classNum);
+  showResult(questionNum);
+
 }
-function showResult(questionId) {
-  alert('the answer was '+answers[questionId].toString())
-  alert('you answered '+useranswers[questionId].toString())
-  alert('did you answer correctly? '+(answers[questionId].toString() == useranswers[questionId].toString()))
-  if(answers[questionId] == useranswers[questionId]) {
+function showResult(questionNum) {
+  alert('the answer was '+answers[questionNum].toString())
+  alert('you answered '+useranswers[questionNum].toString())
+  var1 = String(useranswers[questionNum]).trim(' ')
+  var2 = String(answers[questionNum]).trim(' ')
+
+  // alert(var1 === var2)
+  
+  alert('did you answer correctly? '+(var1 ===var2))
+  if(var1 === var2) {
     // alert('you got it correct')
   correct++;
   } else {
@@ -32,26 +34,21 @@ function showResult(questionId) {
   }
 alert('your total number of correct questions are '+correct)
 }
-function showScore() {
-  questionCount = answers.length;
+function showTotalScore() {
+  totalQuestions = answers.length;
+  message = "You scored " + correct + " out of " + totalQuestions + "\n";
+  alert(message)
   correct = 0;
-  incorrect = 0;
-  for(i=0;i<questionCount;i++) {
-    if(useranswers[i] == answers[i])
-      correct++;
-    else
-      incorrect++;
-  }
-  pc = Math.round((correct / questionCount) * 100);
-  alertMsg = "You scored " + correct + " out of " + questionCount + "\n\n";
-  alert(alertMsg)
-  
-}
-
-function disableQuestion(classId) {
   var alltags=document.all? document.all : document.getElementsByTagName("*")
   for (i=0; i<alltags.length; i++) {
-    if (alltags[i].className == classId) {
+      alltags[i].disabled = false;
+  }
+    } 
+
+function disableQuestion(classNum) {
+  var alltags=document.all? document.all : document.getElementsByTagName("*")
+  for (i=0; i<alltags.length; i++) {
+    if (alltags[i].className == classNum) {
       alltags[i].disabled = true;
     }
   }
