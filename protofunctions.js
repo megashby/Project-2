b3bdb78bc9ea50e5d93a5164808769d4a0ac356f
Meg Ashby <megashby@utexas.edu>
2017-11-12 16:15:30 -0600

    Add files via upload

--- protofunctions.js
+++ protofunctions.js
@@ -3,21 +3,14 @@ var correct = 0;
 
 function createQuiz() {
     for(q=0;q<questions.length;q++) {
-    document.writeln('<p  class="question"><span>'+questions[q]+' <p id="result_'+q+'"</span></p></p>');
+    document.writeln('<p  class="question "blink"><span>'+questions[q]+' <p id="result_'+q+'"</span></p></p>');
     for(a=0;a<3;a++) {
       document.writeln('<p><span><input type="radio" name="answer_ '+q+'" value="'+choices[q][a]+'" id="answer_'+q +'_'+a+'" class="question_'+q+'" onclick="submitQuestion('+q+', this, \'question_'+q+'\')" /><label id="label_'+q+'_'+a+'" for="answer_'+q+'_'+a+'"> '+choices[q][a]+'</label><br /></span></p>');
     }
   }
   document.writeln('<p><input type="submit" value="Show Score" class = "btn btn-info" id= "ad_button" onclick="showTotalScore()" /></p><p style="display:none"></p>');
 }
-//     for(i=0;i<questions.length;i++) {
-//     document.writeln('<p  class="question"><span>'+questions[i]+' <p id="result_'+i+'"</span></p></p>');
-//     for(j=0;j<3;j++) {
-//       document.writeln('<p><span><input type="radio" name="answer_ '+i+'" value="'+choices[i][j]+'" id="answer_'+i +'_'+j+'" class="question_'+i+'" onclick="submitQuestion('+i+', this, \'question_'+i+'\')" /><label id="label_'+i+'_'+j+'" for="answer_'+i+'_'+j+'"> '+choices[i][j]+'</label><br /></span></p>');
-// }
-//   }
-//   document.writeln('<p><input type="submit" value="Show Score" id= "ad_button" class = "btn btn-info" onclick="showTotalScore()" /></p><p style="display:none"></p>');
-// }
+
 
 function submitQuestion(questionNum, obj, classNum) {
   useranswers[questionNum] = obj.value;
@@ -26,16 +19,10 @@ function submitQuestion(questionNum, obj, classNum) {
 
 }
 function showResult(questionNum) {
-
   // alert('the answer was '+answers[questionNum].toString())
   // alert('you answered '+useranswers[questionNum].toString())
   userAns = String(useranswers[questionNum]).trim(' ')
   sysAns = String(answers[questionNum]).trim(' ')
-
-  //alert('the answer was '+answers[questionNum].toString())
-  //alert('you answered '+useranswers[questionNum].toString())
-  var1 = String(useranswers[questionNum]).trim(' ')
-  var2 = String(answers[questionNum]).trim(' ')
 
   // alert(userAns === sysAns)
 
@@ -46,11 +33,7 @@ function showResult(questionNum) {
   } else {
     //alert('you got it incorrect')
   }
-
 alert('your total number of correct questions: '+correct)
-
-  /alert('your total number of correct questions are '+correct)
-
 }
 
 function showTotalScore() {
@@ -62,7 +45,7 @@ function showTotalScore() {
   for (i=0; i<alltags.length; i++) {
       alltags[i].disabled = false;
   }
-    }
+    } 
 
 function disableQuestion(classNum) {
   var alltags=document.all? document.all : document.getElementsByTagName("*")
@@ -83,19 +66,17 @@ function moveToRandomLocation() {
 function startAd() {
   $("#annoying_ad").show().css({top: 0, left: 0});
   $("#annoying_ad").mouseover(moveToRandomLocation);
-  $("#annoying_ad").mousemove(moveToRandomLocation);
 }
 
 // Starts jQuery event listeners (must be called after everything is loaded).
 function setupListeners() {
   $("#ad_button").click(startAd);
+
 }
 
 // This function is automatically called when everything is done loading.
 $(document).ready(function() {
-  // $(".readmore").text("Read More");
-  // $(".details").hide();
   setupListeners();
 });
 
-// shim layer with setTimeout fallback
+
